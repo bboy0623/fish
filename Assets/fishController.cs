c532bffd97947e5658e32e11468fc20b0a7d54e6
aquintiliano <aquintiliano@daitangroup.com>
2014-01-29 22:58:55 -0200

    starting project in unity

---
+++ Assets/fishController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishController : MonoBehaviour {
+
+	public float velocity = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position.Set(transform.position.x + velocity, transform.position.y, transform.position.z);
+	}
+}
